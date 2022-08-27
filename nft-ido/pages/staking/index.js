ea155a92727e5a4895efb7c4f037e3104f1b0b5a
prathibha077 <prathibha@zeroswap.io>
2022-08-27 19:49:53 +0530

    fetch nft functions

--- nft-ido/pages/staking/index.js
+++ nft-ido/pages/staking/index.js
@@ -5,13 +5,29 @@ import React from 'react';
 import {Contract} from "@ethersproject/contracts";
 import { useWeb3React } from "@web3-react/core";
 import {Web3Provider} from "@ethersproject/providers";
-//import { stakeContract } from "../../common/utils";
+import erc721abi from "../../artifacts/erc721abi.json"
+import  stakingabi from "../../artifacts/stakingabi.json"
 export default function staking() {
    
 
     const { chainId, account, activate, active, library } = useWeb3React(); 
     const NFTCONTRACT = "0xaDb5a18F0d6823b1C473a73e92938545583608F4";
     const STAKINGCONTRACT = "0xe8DCcaB339512F86d052558C7515cEb10CDf480b"
+
+     const nftcontract = new Contract(
+        NFTCONTRACT,
+        erc721abi,
+        library && library.getSigner()
+    );
+console.log("nftcontract", nftcontract);
+
+    const stakingcontract = new Contract(
+      STAKINGCONTRACT,
+      stakingabi,
+      library && library.getSigner()
+    );
+
+    console.log("stakingcontract", stakingcontract);
    
     return (
       <div>
