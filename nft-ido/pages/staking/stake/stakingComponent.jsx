ee4d23ac4de8f755088bec3fa7e9844b7c30eb90
prathibha077 <prathibha@zeroswap.io>
2022-08-28 08:05:16 +0530

    added web3 transaction

--- nft-ido/pages/staking/stake/stakingComponent.jsx
+++ nft-ido/pages/staking/stake/stakingComponent.jsx
@@ -25,28 +25,121 @@ export default function StakeComponent() {
   );
 
   const [userNft, setUserNft] = useState();
+  const [stakaeNft, setStakeNft] = useState();
   const [nfttokenID, setNfttokenID] = useState();
   const [metaData, setMetaData] = useState([]);
+  const [checkApprove, setApprove] = useState()
 
   const FetchAcoountNFT = async () => {
     const nftData = await getAlchemyNfts(account);
+    const stakeData = await getAlchemyNfts(STAKINGCONTRACT);
+    setStakeNft(stakeData)
     setUserNft(nftData);
+
     FetchMetaData(nftData);
+    stakeData?.totalCount > 0 && FetchMetaData(stakeData)
   };
 
   const FetchMetaData = async (data) => {
-
-    console.log("nftcontract", await nftcontract.balanceOf(account));
     const arr = [];
     data?.ownedNfts.map(async(item) => {
       arr.push(item.id.tokenId);
+      const nft = await nftcontract.getApproved(item.id.tokenId);
+      setApprove(nft)
       setNfttokenID(arr);
     });
     FetchNFTMetaData(arr);
   };
 
-  
- 
+
+
+  const StakeNFT = async (id) => {
+    const transactionParameters = {
+      to: STAKINGCONTRACT,
+      from: account,
+      data: stakingcontract.interface.encodeFunctionData("stake", [
+        web3.utils.hexToNumber(id)
+      ]),
+    };
+    //sign the transaction via Metamask
+    try {
+      const txHash = await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+      });
+      return {
+        success: true,
+        status:
+          "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+          txHash,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        status: "😥 Something went wrong: " + error.message,
+      };
+    }
+  };
+
+  const UnstakeNFT = async (id) => {
+    const transactionParameters = {
+      to: STAKINGCONTRACT,
+      from: account,
+      data: stakingcontract.interface.encodeFunctionData("unstake", [
+        web3.utils.hexToNumber(id)
+      ]),
+    };
+    //sign the transaction via Metamask
+    try {
+      const txHash = await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+      });
+      return {
+        success: true,
+        status:
+          "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+          txHash,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        status: "😥 Something went wrong: " + error.message,
+      };
+    }
+  };
+
+  const ApproveNFT = async (id) => {
+    const transactionParameters = {
+      to: NFTCONTRACT,
+      from: account,
+      value: parseInt(web3.utils.toWei(String(0), "ether")).toString(
+        16
+      ),
+      data: nftcontract.interface.encodeFunctionData("approve", [
+        STAKINGCONTRACT, 
+        web3.utils.hexToNumber(id)
+      ]),
+    };
+    //sign the transaction via Metamask
+    try {
+      const txHash = await window.ethereum.request({
+        method: "eth_sendTransaction",
+        params: [transactionParameters],
+      });
+      return {
+        success: true,
+        status:
+          "✅ Check out your transaction on Etherscan: https://ropsten.etherscan.io/tx/" +
+          txHash,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        status: "😥 Something went wrong: " + error.message,
+      };
+    }
+  };
 
   const FetchNFTMetaData = async (nfttokenID) => {
     const tokenid = nfttokenID?.map(async (item) => {
@@ -68,7 +161,35 @@ export default function StakeComponent() {
           role="list"
           className="flex justify-center"
         >
-          {metaData?.map((item, index) => (
+          {
+            userNft?.totalCount == 0 ?
+            metaData?.map((item, index) => (
+              <li
+                key={index}
+                className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left m-2"
+              >
+                <div className="flex flex-col justify-center items-center">
+                  <img
+                    className="mx-auto h-40 w-40 rounded-full xl:w-56 xl:h-56"
+                    src={item.metadata.image}
+                    alt=""
+                  />
+                    <div className="font-medium text-lg leading-6 flex flex-col justify-center items-center mt-6">
+                      <h3 className="text-white">{item.metadata.name}</h3>
+                      <p className="text-indigo-400 pt-3">
+                        {item.metadata.description}
+                      </p>
+                    <div className="mt-6">
+                       <button onClick={() => UnstakeNFT(item?.id?.tokenId)} className="bg-sky-100 py-2 px-6 text-gray-600 rounded-3xl">Unstake</button>
+                      
+                    </div>
+                  </div>
+                </div>
+              </li>
+            ))
+             :
+         
+          metaData?.map((item, index) => (
             <li
               key={index}
               className="py-10 px-6 bg-gray-800 text-center rounded-lg xl:px-10 xl:text-left m-2"
@@ -85,12 +206,16 @@ export default function StakeComponent() {
                       {item.metadata.description}
                     </p>
                   <div className="mt-6">
-                    <button className="bg-sky-100 py-2 px-6 text-gray-600 rounded-3xl">Approve</button>
+                    {checkApprove == "0x0000000000000000000000000000000000000000" ?
+                     <button onClick={() => ApproveNFT(item?.id?.tokenId)} className="bg-sky-100 py-2 px-6 text-gray-600 rounded-3xl">Approve</button> :
+                     <button onClick={() => StakeNFT(item?.id?.tokenId)} className="bg-sky-100 py-2 px-6 text-gray-600 rounded-3xl">Stake</button>
+                    }
                   </div>
                 </div>
               </div>
             </li>
-          ))}
+          ))
+        }
         </ul>
       </div>
     </div>
